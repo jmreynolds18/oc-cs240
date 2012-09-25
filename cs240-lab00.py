1c3141c174a1c2a2dbc2384c85718ceee3390ad5
Joshua Reynolds <reyno28@email.lcc.edu>
2012-09-25 08:41:56 -0400

    Added a bird spinning around the room
    Fixed comments, added space, added bird spinning around the room behind
    the olympic flag

--- cs240-lab00.py
+++ cs240-lab00.py
@@ -5,32 +5,67 @@
 # blue background, display an olympic flag, and the ability to quit/exit
 # program.
 
+# Added September 24, 2012 : Added bird image to move across the map
+# and spin at the same time.
 
-import pygame   # imports pygame
 
-pygame.init()   # intializes pygame
+import pygame   
 
-width, height = 640, 480  # sets the screen size
+pygame.init()   
+
+# sets the screen size
+width, height = 640, 480 
 screen = pygame.display.set_mode((width, height))
 
+# Loads image of the bird
+bird = pygame.image.load("bird.gif").convert_alpha()
+bird_rect = bird.get_rect()
 
+
+horizontal = 2
+vertical = 4
+
+# This loop creates bluebackground, bird image to spin
+# olympic flag, and creates an exit program feature
 
 running = True
+while running:
+    # creates blue background                      
+    screen.fill((173, 216, 230)) 
+    
+    # Adds the bird image and spins it
+    screen.blit(bird, bird_rect)    
+    bird = pygame.transform.rotate(bird, 90)
 
-# This while loop rewrites the background, creates the rings for the olympic flag
-# and creates an exit or a quit for the program. 
+    # creates olympic flag                         
+    pygame.draw.rect(screen,(255, 255, 255),(160,100,300,250)) # white rectangle
+    pygame.draw.circle(screen,(0,0,255),(220,180), 50, 3)      # blue ring
+    pygame.draw.circle(screen,(0,0,0),(310,180),50,3)          # black ring    
+    pygame.draw.circle(screen,(255,0,0),(400,180),50,3)        # red ring
+    pygame.draw.circle(screen,(255,255,0),(255,250),50,3)      # yellow ring
+    pygame.draw.circle(screen,(0,255,0),(360,250),50,3)        # green ring
+    
+    # displays the images onto the screen
+    pygame.display.flip()      
 
-while running:                      
-    screen.fill((173, 216, 230)) # Will fill the back ground light blue
-    pygame.draw.rect(screen,(255, 255, 255),(160,100,300,250)) # makes the white rectangle in the middle of the screen
-    pygame.draw.circle(screen,(0,0,255),(220,180), 50, 3) #code for blue ring
-    pygame.draw.circle(screen,(0,0,0),(310,180),50,3)     #code for black ring    
-    pygame.draw.circle(screen,(255,0,0),(400,180),50,3)   #code for red ring
-    pygame.draw.circle(screen,(255,255,0),(255,250),50,3) #code for yellow ring
-    pygame.draw.circle(screen,(0,255,0),(360,250),50,3)   #code for green ring
-    pygame.display.flip()                                 #displays the images onto the screen
-    for event in pygame.event.get():                      #code to exit program
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):#if you press the red x button or press q it will close the program.
+    # Move bird, 1 pixel horizontal, Move bird 1 pixel vertical
+    bird_rect[0] += horizontal      
+    bird_rect[1] += vertical        
+    
+    # this part will move the image across the screen
+    if bird_rect.right >= width:
+        horizontal = -1
+    elif bird_rect.left <= 0:
+        horizontal = 1
+    if bird_rect.bottom >= height:
+        vertical = -3
+    elif bird_rect.top <= 0:
+        vertical = 3
+                                 
+    
+    # code to exit program
+    for event in pygame.event.get():                      
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
             running = False
     
     
