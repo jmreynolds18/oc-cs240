247c11373020720e4eaca3abe497068c1a40282e
Joshua Reynolds <reyno28@email.lcc.edu>
2012-11-06 08:15:27 -0500

    Got the ship to move with keyboard inputs
    The ship now moves with the arrow keys.

--- pygame2.py
+++ pygame2.py
@@ -1,12 +1,14 @@
 # Joshua M Reynolds
 # 10/28/2012
 # Pygame 2
-# This is a space game that creates a space background. Have a ship fly around the map.
-# Create a meteor that moves across the map. The last image is a bird which I still have to figure out
-# what I want that image to do. The ship will have to be movable with the keyboard.
+# This is a space game that creates a space background. Have a ship fly around
+# the map with the keyboard. Insert 2 more images to have a total of 3 images
+# in space. 
+
 
 import random
 import pygame
+from pygame.locals import * 
 
 width, height = 800, 600
 
@@ -63,46 +65,40 @@ def load_ship():
 def main(screen):
     running = True
 
+    # Assign image loads to variables
     bird = load_bird()
     meteor = load_meteor()
     space = build_space(screen)
     ship = load_ship()
-    
-    while running:
-        screen.blit(space, (0, 0))
-        screen.blit(bird, (100, 100))
-        screen.blit(meteor, (200,80))
-        screen.blit(ship, (0,0))
 
-        # Meteor Spinning
-        meteor = pygame.transform.rotate(meteor, 90)
+    # Sets the x and y coordinates for the ship movement
+    x, y = (0,0)
+    while running:
+        # key handle to move ship
+        key = pygame.key.get_pressed()
+        if key[K_LEFT]:
+            x-=1
+        if key[K_RIGHT]:
+            x+=1
+        if key[K_UP]:
+            y-=1
+        if key[K_DOWN]:
+            y+=1
+
+        # display images
+        screen.blit(space, (0, 0))
+        screen.blit(bird, (180, 200))
+        screen.blit(meteor, (200,80))
+        screen.blit(ship, [x,y])        
         pygame.display.flip()
 
-
-        # Not Working, I want the meteor to spin around the map.
-        
-##        pygame.display.flip()           # Display screen in window
-##        # Move meteor, 1 pixel horizontal, Move meteor 1 pixel vertical
-##        meteor[0] += horizontal      
-##        meteor[1] += vertical        
-##        
-##        # bound checking
-##        if meteor.right >= width:
-##            horizontal = -1
-##        elif meteor.left <= 0:
-##            horizontal = 1
-##        if meteor.bottom >= height:
-##            vertical = -3
-##        elif meteor.top <= 0:
-##            vertical = 3
-##        pygame.display.flip()           # Display screen in window
-
         # Code to exit program
-        for event in pygame.event.get():
+        for event in pygame.event.get():                      
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-                # exit()
                 running = False
-
+                
+        
+        
 
 screen = init()
 main(screen)
